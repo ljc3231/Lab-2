11feed489a729a991eec2774d6aecea380d5c2ea
Liam Cummings <ljc3231@rit.edu>
2023-11-01 13:06:31 -0400

    main

---
+++ lab2.py
@@ -0,0 +1,14 @@
+import sys
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 lab2.py <file_path>")
+        return
+
+    file_path = sys.argv[1]
+
+    
+    
+
+if __name__ == "__main__":
+    main()
